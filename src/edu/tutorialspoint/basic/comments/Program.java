fec429181964aa01af9caf339dfbe9470320980d
allecosta <alle_costa10@outlook.com>
2024-10-16 18:13:16 -0300

    feat: added comments

---
+++ src/edu/tutorialspoint/basic/comments/Program.java
@@ -0,0 +1,27 @@
+package edu.tutorialspoint.basic.comments;
+
+public class Program {
+
+    /**
+     * @param dividend
+     * @param divisor
+     * @return quotient
+     * @throws IllegalArgumentException
+     */
+    public double divide(int dividend, int divisor) throws IllegalArgumentException {
+        if ((dividend == 0) || (divisor == 0)) {
+            throw new IllegalArgumentException("OPS! Não se pode dividir por 0.");
+        } 
+
+        double result = dividend / divisor;
+        
+        return result; 
+    }
+
+    public static void main(String[] args) {
+        Program program = new Program();
+        double result = program.divide(25, 5);
+
+        System.out.println(result);
+    }
+}
